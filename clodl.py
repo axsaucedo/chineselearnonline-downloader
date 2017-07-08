bac25235cc423b112b2591d3a722456bc1e56c55
Alejandro Saucedo <axsauze@gmail.com>
2017-07-08 14:32:56 +0100

    added files

--- clodl.py
+++ clodl.py
@@ -1,11 +1,29 @@
 
 from urllib.request import urlretrieve
-import sys
-import os
+import sys, os, shutil
 
+FOLDER="download"
 TOTAL_LESSONS=420
-BASE_URL="https://www.chineselearnonline.com/wp-content/uploads/ChineseLearnOnline_"
-EXT=".mp3"
+AUDIO_EXT=".mp3"
+FILE_EXT=".pdf"
+
+def url_audio(name):
+    return f"https://www.chineselearnonline.com/wp-content/uploads/ChineseLearnOnline_{name}{AUDIO_EXT}"
+
+def url_vocab(name):
+    return f"https://www.chineselearnonline.com/premium/CLO_{name}_Vocab.pdf"
+
+def url_pinyin(name):
+    return f"https://www.chineselearnonline.com/premium/CLO_{name}_Comp_P.pdf"
+
+def url_simplified(name):
+    return f"https://www.chineselearnonline.com/premium/CLO_{name}_Comp_S.pdf"
+
+def url_traditional(name):
+    return f"https://www.chineselearnonline.com/premium/CLO_{name}_Comp_T.pdf"
+
+def url_english(name):
+    return f"https://www.chineselearnonline.com/premium/CLO_{name}_Comp_E.pdf"
 
 def reporthook(blocknum, blocksize, totalsize):
     readsofar = blocknum * blocksize
@@ -20,37 +38,67 @@ def reporthook(blocknum, blocksize, totalsize):
         sys.stderr.write("read %d\n" % (readsofar,))
 
 def start_previous_to_last():
-    files = os.listdir()
-    names = ["".join(f.split(".")[:-1]) for f in files]
-    numbers = [n for n in names if n.isdigit()]
+    files = os.listdir("FOLDER")
+    numbers = [n for n in files if n.isdigit()]
 
     if not numbers:
         return 1
 
     nums = [int(n) for n in numbers]
     latest = max(nums)
-    filename = str(latest).zfill(3)
-    file = f"{filename}{EXT}"
-    os.remove(file)
+    foldername = str(latest).zfill(3)
+
+    # Remove the latest one as it may be incomplete / corrupted
+    shutil.rmtree(foldername)
+
     return latest
 
-def download_files(start, total):
+def create_folder_if_not_exists(fname):
+    if not os.path.exists(fname):
+        return os.makedirs(fname)
 
-    for i in range(start,total+1):
-        name = str(i).zfill(3)
+def download_audio(num, base):
 
-        print(f"Downloading {name}{EXT}")
+    name = str(num).zfill(3)
 
-        url = f"{BASE_URL}{name}{EXT}"
+    print(f"Downloading {name}{AUDIO_EXT}")
 
-        urlretrieve(url, f"{name}{EXT}", reporthook)
+    url = url_audio(name)
+
+    BASE_FILE_URL_V=""
+
+    urlretrieve(url, f"{folder_name}/{name}{AUDIO_EXT}", reporthook)
+
+def download_files(num, folder_name):
+
+    name = str(num).zfill(3)
+    print(f"Downloading {name} files")
+    v = url_vocab(name)
+    p = url_pinyin(name)
+    s = url_simplified(name)
+    t = url_traditional(name)
+    e = url_english(name)
+
+    urlretrieve(v, f"{folder_name}/{name}_Vocab{FILE_EXT}", reporthook)
+    urlretrieve(p, f"{folder_name}/{name}_Pinyin{FILE_EXT}", reporthook)
+    urlretrieve(s, f"{folder_name}/{name}_Simplified{FILE_EXT}", reporthook)
+    urlretrieve(t, f"{folder_name}/{name}_Traditional{FILE_EXT}", reporthook)
+    urlretrieve(e, f"{folder_name}/{name}_English{FILE_EXT}", reporthook)
 
 if __name__ == "__main__":
+
+    create_folder_if_not_exists(FOLDER)
 
     start = start_previous_to_last()
     print(f"Starting from {start}")
 
-    download_files(start, TOTAL_LESSONS)
+    for i in range(start, TOTAL_LESSONS + 1):
+        fname = str(num).zfill(3)
+        base_folder = f"{FOLDER}/{fname}"
+
+        download_audio(i, base_folder)
+        download_files(i, base_folder)
+
 
 
 
